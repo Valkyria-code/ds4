abdbcb761c98bf0f9e8a9e01bfbd4a506230c7a3
Valkyria-code <valeriejezieldgg@gmail.com>
2025-09-16 19:42:44 -0500

    Subir laboratorio 2

---
+++ Laboratorio 2/Laboratorio 2-1/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        MyClass.Valor = 1;
+        Console.WriteLine(MyClass.Valor);
+    }
+}
+
+public class MyClass
+{
+    public static int Valor;
+}
---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.FirstName = "Su nombre";
+        client.LastName = "Su apellido";
+        client.Age = 15;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+}
+
+public class Client
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public ushort Age { get; set; }
+
+    public string GetFullName()
+    {
+        return FirstName + " " + LastName;
+    }
+}
