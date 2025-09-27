d0df36d8100931b7f053459c1764b2e0bea59a69
Val-Code <valeriejezieldgg@gmail.com>
2025-09-27 09:26:38 -0500

    docs: subir laboratorio 7

---
+++ Laboratorio 7/Laboratorio 7-1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Laboratorio_7_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        JuegoDeDados j = new JuegoDeDados();
+
+        j.Jugar();
+    }
+}
---
+++ Laboratorio 7/Laboratorio 7/Program.cs
@@ -0,0 +1,14 @@
+﻿using Laboratorio_7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Banco banco1 = new Banco();
+
+        banco1.Operar();
+        banco1.DepositosTotales();
+
+        Console.ReadKey();
+    }
+}
