1f908f70feea2fe740f3b77a0cae9f39e1778e57
Valkyria-code <valeriejezieldgg@gmail.com>
2025-10-29 02:33:14 -0500

    subir parcial 2

---
+++ Parcial 2/Parcial 2/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Parcial_2
+{
+    partial class ventana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.lbl_hex = new System.Windows.Forms.Label();
+            this.btn_hex = new System.Windows.Forms.Button();
+            this.lbl_dec1 = new System.Windows.Forms.Label();
+            this.lbl_oc = new System.Windows.Forms.Label();
+            this.lbl_dec2 = new System.Windows.Forms.Label();
+            this.tbx_hex = new System.Windows.Forms.TextBox();
+            this.tbx_dec1 = new System.Windows.Forms.TextBox();
+            this.tbx_dec2 = new System.Windows.Forms.TextBox();
+            this.tbx_oc = new System.Windows.Forms.TextBox();
+            this.tbx_messagehex = new System.Windows.Forms.TextBox();
+            this.tbx_messagedec1 = new System.Windows.Forms.TextBox();
+            this.tbx_messageoc = new System.Windows.Forms.TextBox();
+            this.tbx_messagedec2 = new System.Windows.Forms.TextBox();
+            this.btn_dec1 = new System.Windows.Forms.Button();
+            this.btn_dec2 = new System.Windows.Forms.Button();
+            this.btn_oc = new System.Windows.Forms.Button();
+            this.dgvHistorial = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titulo.Location = new System.Drawing.Point(152, 26);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(237, 26);
+            this.lbl_titulo.TabIndex = 0;
+            this.lbl_titulo.Text = "Conversor Numérico ";
+            this.lbl_titulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbl_titulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_hex
+            // 
+            this.lbl_hex.AutoSize = true;
+            this.lbl_hex.Location = new System.Drawing.Point(24, 81);
+            this.lbl_hex.Name = "lbl_hex";
+            this.lbl_hex.Size = new System.Drawing.Size(118, 13);
+            this.lbl_hex.TabIndex = 1;
+            this.lbl_hex.Text = "Hexadecimal a Decimal";
+            this.lbl_hex.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_hex
+            // 
+            this.btn_hex.Location = new System.Drawing.Point(314, 76);
+            this.btn_hex.Name = "btn_hex";
+            this.btn_hex.Size = new System.Drawing.Size(75, 23);
+            this.btn_hex.TabIndex = 2;
+            this.btn_hex.Text = "Convertir";
+            this.btn_hex.UseVisualStyleBackColor = true;
+            this.btn_hex.Click += new System.EventHandler(this.btn_hecdec_Click);
+            // 
+            // lbl_dec1
+            // 
+            this.lbl_dec1.AutoSize = true;
+            this.lbl_dec1.Location = new System.Drawing.Point(24, 127);
+            this.lbl_dec1.Name = "lbl_dec1";
+            this.lbl_dec1.Size = new System.Drawing.Size(118, 13);
+            this.lbl_dec1.TabIndex = 3;
+            this.lbl_dec1.Text = "Decimal a Hexadecimal";
+            // 
+            // lbl_oc
+            // 
+            this.lbl_oc.AutoSize = true;
+            this.lbl_oc.Location = new System.Drawing.Point(24, 175);
+            this.lbl_oc.Name = "lbl_oc";
+            this.lbl_oc.Size = new System.Drawing.Size(82, 13);
+            this.lbl_oc.TabIndex = 4;
+            this.lbl_oc.Text = "Octal a Decimal";
+            // 
+            // lbl_dec2
+            // 
+            this.lbl_dec2.AutoSize = true;
+            this.lbl_dec2.Location = new System.Drawing.Point(24, 219);
+            this.lbl_dec2.Name = "lbl_dec2";
+            this.lbl_dec2.Size = new System.Drawing.Size(82, 13);
+            this.lbl_dec2.TabIndex = 5;
+            this.lbl_dec2.Text = "Decimal a Octal";
+            // 
+            // tbx_hex
+            // 
+            this.tbx_hex.Location = new System.Drawing.Point(165, 78);
+            this.tbx_hex.Name = "tbx_hex";
+            this.tbx_hex.Size = new System.Drawing.Size(100, 20);
+            this.tbx_hex.TabIndex = 6;
+            // 
+            // tbx_dec1
+            // 
+            this.tbx_dec1.Location = new System.Drawing.Point(165, 124);
+            this.tbx_dec1.Name = "tbx_dec1";
+            this.tbx_dec1.Size = new System.Drawing.Size(100, 20);
+            this.tbx_dec1.TabIndex = 7;
+            // 
+            // tbx_dec2
+            // 
+            this.tbx_dec2.Location = new System.Drawing.Point(165, 216);
+            this.tbx_dec2.Name = "tbx_dec2";
+            this.tbx_dec2.Size = new System.Drawing.Size(100, 20);
+            this.tbx_dec2.TabIndex = 8;
+            // 
+            // tbx_oc
+            // 
+            this.tbx_oc.Location = new System.Drawing.Point(165, 172);
+            this.tbx_oc.Name = "tbx_oc";
+            this.tbx_oc.Size = new System.Drawing.Size(100, 20);
+            this.tbx_oc.TabIndex = 9;
+            // 
+            // tbx_messagehex
+            // 
+            this.tbx_messagehex.Location = new System.Drawing.Point(437, 78);
+            this.tbx_messagehex.Name = "tbx_messagehex";
+            this.tbx_messagehex.ReadOnly = true;
+            this.tbx_messagehex.Size = new System.Drawing.Size(100, 20);
+            this.tbx_messagehex.TabIndex = 10;
+            this.tbx_messagehex.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbx_messagedec1
+            // 
+            this.tbx_messagedec1.Location = new System.Drawing.Point(437, 124);
+            this.tbx_messagedec1.Name = "tbx_messagedec1";
+            this.tbx_messagedec1.ReadOnly = true;
+            this.tbx_messagedec1.Size = new System.Drawing.Size(100, 20);
+            this.tbx_messagedec1.TabIndex = 11;
+            this.tbx_messagedec1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbx_messageoc
+            // 
+            this.tbx_messageoc.Location = new System.Drawing.Point(437, 172);
+            this.tbx_messageoc.Name = "tbx_messageoc";
+            this.tbx_messageoc.ReadOnly = true;
+            this.tbx_messageoc.Size = new System.Drawing.Size(100, 20);
+            this.tbx_messageoc.TabIndex = 12;
+            this.tbx_messageoc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbx_messagedec2
+            // 
+            this.tbx_messagedec2.Location = new System.Drawing.Point(437, 216);
+            this.tbx_messagedec2.Name = "tbx_messagedec2";
+            this.tbx_messagedec2.ReadOnly = true;
+            this.tbx_messagedec2.Size = new System.Drawing.Size(100, 20);
+            this.tbx_messagedec2.TabIndex = 13;
+            this.tbx_messagedec2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_dec1
+            // 
+            this.btn_dec1.Location = new System.Drawing.Point(314, 122);
+            this.btn_dec1.Name = "btn_dec1";
+            this.btn_dec1.Size = new System.Drawing.Size(75, 23);
+            this.btn_dec1.TabIndex = 14;
+            this.btn_dec1.Text = "Convertir";
+            this.btn_dec1.UseVisualStyleBackColor = true;
+            this.btn_dec1.Click += new System.EventHandler(this.btn_dechec_Click);
+            // 
+            // btn_dec2
+            // 
+            this.btn_dec2.Location = new System.Drawing.Point(314, 214);
+            this.btn_dec2.Name = "btn_dec2";
+            this.btn_dec2.Size = new System.Drawing.Size(75, 23);
+            this.btn_dec2.TabIndex = 15;
+            this.btn_dec2.Text = "Convertir";
+            this.btn_dec2.UseVisualStyleBackColor = true;
+            this.btn_dec2.Click += new System.EventHandler(this.btn_decoc_Click);
+            // 
+            // btn_oc
+            // 
+            this.btn_oc.Location = new System.Drawing.Point(314, 170);
+            this.btn_oc.Name = "btn_oc";
+            this.btn_oc.Size = new System.Drawing.Size(75, 23);
+            this.btn_oc.TabIndex = 16;
+            this.btn_oc.Text = "Convertir";
+            this.btn_oc.UseVisualStyleBackColor = true;
+            this.btn_oc.Click += new System.EventHandler(this.btn_ocdec_Click);
+            // 
+            // dgvHistorial
+            // 
+            this.dgvHistorial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistorial.Location = new System.Drawing.Point(12, 296);
+            this.dgvHistorial.Name = "dgvHistorial";
+            this.dgvHistorial.Size = new System.Drawing.Size(542, 150);
+            this.dgvHistorial.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Historial";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // ventana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 466);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvHistorial);
+            this.Controls.Add(this.btn_oc);
+            this.Controls.Add(this.btn_dec2);
+            this.Controls.Add(this.btn_dec1);
+            this.Controls.Add(this.tbx_messagedec2);
+            this.Controls.Add(this.tbx_messageoc);
+            this.Controls.Add(this.tbx_messagedec1);
+            this.Controls.Add(this.tbx_messagehex);
+            this.Controls.Add(this.tbx_oc);
+            this.Controls.Add(this.tbx_dec2);
+            this.Controls.Add(this.tbx_dec1);
+            this.Controls.Add(this.tbx_hex);
+            this.Controls.Add(this.lbl_dec2);
+            this.Controls.Add(this.lbl_oc);
+            this.Controls.Add(this.lbl_dec1);
+            this.Controls.Add(this.btn_hex);
+            this.Controls.Add(this.lbl_hex);
+            this.Controls.Add(this.lbl_titulo);
+            this.Name = "ventana";
+            this.Text = "Conversor Numérico";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.Label lbl_hex;
+        private System.Windows.Forms.Button btn_hex;
+        private System.Windows.Forms.Label lbl_dec1;
+        private System.Windows.Forms.Label lbl_oc;
+        private System.Windows.Forms.Label lbl_dec2;
+        private System.Windows.Forms.TextBox tbx_hex;
+        private System.Windows.Forms.TextBox tbx_dec1;
+        private System.Windows.Forms.TextBox tbx_dec2;
+        private System.Windows.Forms.TextBox tbx_oc;
+        private System.Windows.Forms.TextBox tbx_messagehex;
+        private System.Windows.Forms.TextBox tbx_messagedec1;
+        private System.Windows.Forms.TextBox tbx_messageoc;
+        private System.Windows.Forms.TextBox tbx_messagedec2;
+        private System.Windows.Forms.Button btn_dec1;
+        private System.Windows.Forms.Button btn_dec2;
+        private System.Windows.Forms.Button btn_oc;
+        private System.Windows.Forms.DataGridView dgvHistorial;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Parcial 2/Parcial 2/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_2
+{
+    public partial class ventana : Form
+    {
+
+        string connectionString = @"Server=.;Database=ConversorNumerico;TrustServerCertificate=true;Integrated Security=SSPI;";
+
+        public ventana()
+        {
+            InitializeComponent();
+            CargarHistorial();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GuardarConversion(string tipoConversion, string valorEntrada, string valorSalida)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"INSERT INTO Historial (TipoConversion, ValorEntrada, ValorSalida, FechaHora) VALUES (@Tipo, @Entrada, @Salida, @Fecha)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Tipo", tipoConversion);
+                        cmd.Parameters.AddWithValue("@Entrada", valorEntrada);
+                        cmd.Parameters.AddWithValue("@Salida", valorSalida);
+                        cmd.Parameters.AddWithValue("@Fecha", DateTime.Now);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                CargarHistorial();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar en la base de datos:\n{ex.Message}", "Error de BD");
+            }
+        }
+
+        private void CargarHistorial()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"SELECT Id, TipoConversion, ValorEntrada, ValorSalida, FechaHora FROM Historial ORDER BY FechaHora DESC";
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        dgvHistorial.DataSource = dt;
+
+                        if (dgvHistorial.Columns.Count > 0)
+                        {
+                            dgvHistorial.Columns["Id"].HeaderText = "ID";
+                            dgvHistorial.Columns["TipoConversion"].HeaderText = "Tipo";
+                            dgvHistorial.Columns["ValorEntrada"].HeaderText = "Entrada";
+                            dgvHistorial.Columns["ValorSalida"].HeaderText = "Salida";
+                            dgvHistorial.Columns["FechaHora"].HeaderText = "Fecha y Hora";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar el historial:\n{ex.Message}", "Error de BD");
+            }
+        }
+
+        private void btn_hecdec_Click(object sender, EventArgs e)
+        {
+            string valorHexadecimal = tbx_hex.Text;
+
+            if (string.IsNullOrWhiteSpace(valorHexadecimal))
+            {
+                MessageBox.Show("Por favor ingresa un valor hexadecimal.", "Campo vacío");
+
+                return;
+            }
+
+            try
+            {
+                int resultado = Convert.ToInt32(valorHexadecimal, 16);
+                tbx_messagehex.Text = resultado.ToString();
+
+                GuardarConversion("Hexadecimal a Decimal", valorHexadecimal.ToUpper(), resultado.ToString());
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El valor ingresado no es un hexadecimal válido.\n" + "Usa solo: 0-9, A-F", "Error de formato");
+
+                // Limpiar el TextBox
+                tbx_hex.Clear();
+                tbx_hex.Focus();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("El número es demasiado grande.", "Error");
+            }
+        }
+
+        private void btn_dechec_Click(object sender, EventArgs e)
+        {
+            string valorDecimal = tbx_dec1.Text;
+
+            if (string.IsNullOrWhiteSpace(valorDecimal))
+            {
+                MessageBox.Show("Por favor ingresa un valor decimal.", "Campo vacío");
+
+                return;
+            }
+
+            try
+            {
+                int valorDec = int.Parse(valorDecimal);
+                string resultado = Convert.ToString(valorDec, 16).ToUpper();
+
+                tbx_messagedec1.Text = resultado;
+
+                GuardarConversion("Decimal a Hexadecimal", valorDecimal.ToUpper(), resultado.ToString());
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El valor ingresado no es un número decimal válido.\n" + "Usa solo: 0-9", "Error de formato");
+                
+                tbx_dec1.Clear();
+                tbx_dec1.Focus();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("El número está fuera del rango permitido.", "Error");
+            }
+        }
+
+        private void btn_ocdec_Click(object sender, EventArgs e)
+        {
+            string valorOctal = tbx_oc.Text;
+
+            if (string.IsNullOrWhiteSpace(valorOctal))
+            {
+                MessageBox.Show("Por favor ingresa un valor octal.", "Campo vacío");
+                
+                return;
+            }
+
+            try
+            {
+                int resultado = Convert.ToInt32(valorOctal, 8);
+
+                tbx_messageoc.Text = resultado.ToString();
+
+                GuardarConversion("Octal a Decimal", valorOctal.ToUpper(), resultado.ToString());
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El valor ingresado no es un octal válido.\n" + "Usa solo: 0-7", "Error de formato");
+                
+                tbx_oc.Clear();
+                tbx_oc.Focus();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("El número es demasiado grande.", "Error");
+            }
+        }
+
+        private void btn_decoc_Click(object sender, EventArgs e)
+        {
+            string valorDecimal2 = tbx_dec2.Text;
+
+            if (string.IsNullOrWhiteSpace(valorDecimal2))
+            {
+                MessageBox.Show("Por favor ingresa un valor decimal.", "Campo vacío");
+                
+                return;
+            }
+
+            try
+            {
+                int numero = int.Parse(valorDecimal2);
+                string resultado = Convert.ToString(numero, 8);
+                tbx_messagedec2.Text = resultado;
+
+                GuardarConversion("Decimal a Octal", valorDecimal2, resultado);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El valor ingresado no es un número decimal válido.\nUsa solo: 0-9", "Error de formato");
+                
+                tbx_dec2.Clear();
+                tbx_dec2.Focus();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("El número está fuera del rango permitido.", "Error");
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
